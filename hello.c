0b89d2d516b7b1b25bfa1e2818f0798c00f43059
stefanoaragoni <stefanoaragoni13@gmail.com>
2023-08-11 20:38:29 -0600

    programa1

--- hello.c
+++ hello.c
@@ -1,21 +1,26 @@
+// Stefano Alberto Aragoni Maldonado (20261)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "omp.h"
 
-void hello_world(void);
+void hello_world(int thread_num, int thread_count);
     
 int main(int argc, char* argv[]) {
     int thread_count = 10;
     
-    if(argc > 1){
+    // Revisa que se haya ingresado un valor.
+    if (argc > 1) {
         thread_count = strtol(argv[1], NULL, 10);
     }
 
     #pragma omp parallel num_threads(thread_count)
-    hello_world();
+    hello_world(omp_get_thread_num(), thread_count);    // Pasa como parametro el numero de hilo actual y la cantidad de hilos
+
     return 0;
 }
 
-void hello_world(void) {
-    printf("Hello World!\n");
+void hello_world(int thread_num, int thread_count) {
+    // Recibe el numero de hilo actual, asi como la cantidad de hilos
+    printf("Hello from thread %d of %d!\n", thread_num, thread_count);      // Imprime la cantidad de hilos
 }
